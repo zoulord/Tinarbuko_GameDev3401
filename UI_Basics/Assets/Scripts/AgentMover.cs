9665f52cf4a2132d5a1c16342817c6dbc875b379
Zul Tinarbuko <zt421918@scc-mmlab-02.local>
2020-02-10 16:41:31 -0500

    Added the project

---
+++ UI_Basics/Assets/Scripts/AgentMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMover : MonoBehaviour
+{
+    // variables
+    [Header("Navigation Components")]
+    public NavMeshAgent agent;
+    public Transform moveTarget;
+    public float defaultMoveSpeed = 10;
+
+    public bool doMoveAtDoubleSpeed;
+    public int myInteger = -50;
+    public float myFloat = 4.5f;
+    public string myString = "hello world";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(moveTarget.position);
+
+        if (doMoveAtDoubleSpeed == true)
+        {
+            // code that executes ONLY IF conditional is TRUE
+            agent.speed = defaultMoveSpeed * 2;
+        }
+        else
+        {
+            agent.speed = defaultMoveSpeed;
+        }
+    }
+}
---
+++ UI_Basics/Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public AudioSource buttonAudioSource;
+
+
+
+    public void OnButtonClicked ()
+    {
+        if (buttonAudioSource.isPlaying)
+        {
+            buttonAudioSource.Stop();
+        }
+        else
+        {
+            buttonAudioSource.Play();
+        }
+    }
+}
